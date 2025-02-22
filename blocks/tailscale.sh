767c048e915eb9e96e677bb6bb11415141ba4f5a
yahorni <neuralorge@gmail.com>
2025-02-22 12:00:38 +0300

    Clean up tailscale.sh block

--- blocks/tailscale.sh
+++ blocks/tailscale.sh
@@ -2,8 +2,8 @@
 
 case $BLOCK_BUTTON in
     1)
-        if systemctl is-active tailscaled >/dev/null 2>&1 ; then
-            if tailscale status >/dev/null 2>&1 ; then
+        if systemctl is-active tailscaled ; then
+            if tailscale status ; then
                 status_msg="ipv4 $(tailscale ip -4)\n$(grep nameserver /etc/resolv.conf)"
             else
                 status_msg="client: stopped"
@@ -13,27 +13,28 @@ case $BLOCK_BUTTON in
             notify-send "👾 Tailscale" "daemon: stopped"
         fi
         ;;
-    2) set -x
+    2)
         notify-send "👾 Tailscale" "Restarting"
-        if ! systemctl is-active tailscaled >/dev/null 2>&1 ; then
-            sudo -A systemctl start tailscaled >/dev/null 2>&1
-        elif tailscale status >/dev/null 2>&1 ; then
-            sudo -A tailscale down >/dev/null 2>&1
+        if ! systemctl is-active tailscaled ; then
+            sudo -A systemctl start tailscaled
+        elif tailscale status ; then
+            sudo -A tailscale down
         fi
-        sudo -A tailscale up >/dev/null 2>&1
+        sudo -A tailscale up --accept-routes
         ;;
     3)
-        if tailscale status >/dev/null 2>&1 ; then
-            sudo -A tailscale down >/dev/null 2>&1 &&\
-                sudo -A systemctl stop tailscaled >/dev/null 2>&1
+        notify-send "👾 Tailscale" "Toggling"
+        if tailscale status ; then
+            sudo -A tailscale down &&\
+                sudo -A systemctl stop tailscaled
         else
-            sudo -A systemctl start tailscaled >/dev/null 2>&1 && \
-                sudo -A tailscale up >/dev/null 2>&1
+            sudo -A systemctl start tailscaled && \
+                sudo -A tailscale up --accept-routes
         fi
         ;;
-esac
+esac >/dev/null 2>&1
 
-if tailscale status >/dev/null 2>&1 ; then 
+if tailscale status >/dev/null 2>&1 ; then
     echo "👾 $(tailscale ip -4)"
 else
     echo "👾 ⏹"
