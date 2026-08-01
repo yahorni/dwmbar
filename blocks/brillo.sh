910a843d373735d9e3c6e097bb8fb500e918f65f
yahorni <neuralorge@gmail.com>
2026-08-01 23:52:22 +0300

    Update scripts

--- blocks/brillo.sh
+++ blocks/brillo.sh
@@ -2,10 +2,12 @@
 set -e
 
 case "$BLOCK_BUTTON" in
-    1) brillo -S 75 ;;
-    3) brillo -S 100 ;;
-    4) brillo -q -U 1 ;;
-    5) brillo -q -A 1 ;;
+    1)  brillo -S 50 ;;
+    3)  brillo -S 100 ;;
+    4)  brillo -U 1 -q ;;
+    5)  brillo -A 1 -q ;;
+    9)  brillo -U 1 ;;
+    10) brillo -A 1 ;;
 esac
 
 echo "🌞 $(brillo | cut -d'.' -f1)"
